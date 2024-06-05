cd64f67f6938cd421156e2d5acd603e88f5a4c67
Th3-l4dy <f.bencharef@esi-sba.dz>
2024-06-05 11:17:45 +0100

    last changes

--- lib/components/product_type_dialog.dart
+++ lib/components/product_type_dialog.dart
@@ -1,3 +1,124 @@
+// import 'dart:math';
+// import 'package:flutter/material.dart';
+// import 'package:http/http.dart' as http;
+// import 'dart:convert';
+// import 'package:shared_preferences/shared_preferences.dart';
+// import 'package:flutter_dotenv/flutter_dotenv.dart';
+// import 'package:flutter_application_1/providers/cart_provider.dart';
+
+// String backendIP = dotenv.env['BACKEND_IP']!;
+
+// class ProductTypeDialog extends StatefulWidget {
+//   final VoidCallback onDecharge;
+//   final VoidCallback onInterne;
+//   final CartProvider cartProvider;
+
+//   const ProductTypeDialog({
+//     Key? key,
+//     required this.onDecharge,
+//     required this.onInterne,
+//     required this.cartProvider,
+//   }) : super(key: key);
+
+//   @override
+//   _ProductTypeDialogState createState() => _ProductTypeDialogState();
+// }
+
+// class _ProductTypeDialogState extends State<ProductTypeDialog> {
+//   String? _commandType;
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return AlertDialog(
+//       title: Text('Select Command Type'),
+//       content: Column(
+//         mainAxisSize: MainAxisSize.min,
+//         children: [
+//           ElevatedButton(
+//             onPressed: () {
+//               widget.onDecharge();
+//               setState(() {
+//                 _commandType = 'Commande Decharges';
+//               });
+//             },
+//             child: Text('Decharge'),
+//           ),
+//           SizedBox(height: 10),
+//           ElevatedButton(
+//             onPressed: () {
+//               widget.onInterne();
+//               setState(() {
+//                 _commandType = 'Commande Interne';
+//               });
+//             },
+//             child: Text('Interne'),
+//           ),
+//           SizedBox(height: 16),
+//           ElevatedButton(
+            
+//             onPressed: _commandType != null
+//                 ? () {
+//                     _createCommand(context);
+//                   }
+//                 : null,
+              
+//             child: Text('Create Command'),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+
+//   Future<void> _createCommand(BuildContext context) async {
+//     final prefs = await SharedPreferences.getInstance();
+//     final _userId = prefs.getInt('id');
+//     try {
+//       final url = Uri.parse('http://$backendIP:4000/api/bons/create-bon-commande-interne/$_userId');
+//       final produitsCommandes = widget.cartProvider.products.map((product) {
+//         final quantity = widget.cartProvider.getProductQuantity(product);
+//         return {
+//           'id_produit': product.id,
+//           'orderedquantity': quantity,
+//         };
+//       }).toList();
+
+//       final response = await http.post(
+//         url,
+//         headers: {'Content-Type': 'application/json'},
+//         body: json.encode({
+//           'number': _generateRandomNumber(),
+//           'date': _getCurrentDate(),
+//           'produitsCommandes': produitsCommandes,
+//           'typecommande': _commandType,
+//         }),
+//       );
+
+//       if (response.statusCode == 200) {
+//         // Command created successfully
+//         print('Command created successfully');
+//         Navigator.of(context).pop();
+//       } else {
+//         // Handle error
+//         print('Error creating command: ${response.statusCode}');
+//       }
+//     } catch (e) {
+//       print('Error creating command: $e');
+//     }
+//   }
+
+//   String _generateRandomNumber() {
+//     final randomNumber = 100000 + Random().nextInt(900000);
+//     return randomNumber.toString();
+//   }
+
+//   String _getCurrentDate() {
+//     final now = DateTime.now();
+//     return '${now.year}-${now.month.toString().padLeft(2, '0')}-${now.day.toString().padLeft(2, '0')}';
+//   }
+// }
+
+
+
 import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
@@ -30,7 +151,7 @@ class _ProductTypeDialogState extends State<ProductTypeDialog> {
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
-      title: Text('Select Product Type'),
+      title: Text('Select Command Type'),
       content: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
@@ -41,7 +162,11 @@ class _ProductTypeDialogState extends State<ProductTypeDialog> {
                 _commandType = 'Commande Decharges';
               });
             },
-            child: Text('Decharge'),
+            style: ElevatedButton.styleFrom(
+              
+              // backgroundColor: Colors.blue, // Change the color of the button
+            ),
+            child: Text('Decharge',style: TextStyle(color: Colors.black),),
           ),
           SizedBox(height: 10),
           ElevatedButton(
@@ -51,16 +176,23 @@ class _ProductTypeDialogState extends State<ProductTypeDialog> {
                 _commandType = 'Commande Interne';
               });
             },
-            child: Text('Interne'),
+            style: ElevatedButton.styleFrom(
+              // backgroundColor: Colors.green, // Change the color of the button
+            ),
+            child: Text('Interne',style: TextStyle(color: Colors.black),),
           ),
           SizedBox(height: 16),
           ElevatedButton(
             onPressed: _commandType != null
-                ? () {
-                    _createCommand(context);
+                ? () async {
+                    await _createCommand(context);
+                    Navigator.of(context).pop(); // Close the dialog
                   }
                 : null,
-            child: Text('Create Command'),
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.green, // Change the color of the button
+            ),
+            child: Text('Create Command',style: TextStyle(color: Colors.white),),
           ),
         ],
       ),
@@ -94,7 +226,6 @@ class _ProductTypeDialogState extends State<ProductTypeDialog> {
       if (response.statusCode == 200) {
         // Command created successfully
         print('Command created successfully');
-        Navigator.of(context).pop();
       } else {
         // Handle error
         print('Error creating command: ${response.statusCode}');
--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,3 +1,5 @@
+
+
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_application_1/providers/cart_provider.dart';
@@ -14,13 +16,11 @@ class CartPage extends StatelessWidget {
       builder: (BuildContext context) {
         return ProductTypeDialog(
           onDecharge: () {
-            // Handle "decharge" product type selection
             ScaffoldMessenger.of(context).showSnackBar(
               SnackBar(content: Text('Decharge selected')),
             );
           },
           onInterne: () {
-            // Handle "interne" product type selection
             ScaffoldMessenger.of(context).showSnackBar(
               SnackBar(content: Text('Interne selected')),
             );
@@ -36,7 +36,14 @@ class CartPage extends StatelessWidget {
     final cartProvider = Provider.of<CartProvider>(context);
     return Scaffold(
       appBar: AppBar(
-        title: Text('Cart'),
+        title: Text(
+          'Cart',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 24,
+          ),
+        ),
+        centerTitle: true,
         actions: [
           Stack(
             alignment: Alignment.center,
@@ -55,20 +62,21 @@ class CartPage extends StatelessWidget {
                   right: 8,
                   top: 8,
                   child: Container(
-                    padding: EdgeInsets.all(2),
+                    padding: EdgeInsets.all(4),
                     decoration: BoxDecoration(
                       color: Colors.red,
                       borderRadius: BorderRadius.circular(10),
                     ),
                     constraints: BoxConstraints(
-                      minWidth: 16,
-                      minHeight: 16,
+                      minWidth: 20,
+                      minHeight: 20,
                     ),
                     child: Text(
                       cartProvider.products.length.toString(),
                       style: TextStyle(
                         color: Colors.white,
-                        fontSize: 10,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 12,
                       ),
                       textAlign: TextAlign.center,
                     ),
@@ -94,7 +102,8 @@ class CartPage extends StatelessWidget {
           ),
           Padding(
             padding: const EdgeInsets.all(16.0),
-            child: Column(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
                 ElevatedButton(
                   onPressed: () {
@@ -102,18 +111,39 @@ class CartPage extends StatelessWidget {
                   },
                   style: ElevatedButton.styleFrom(
                     backgroundColor: Colors.red,
+                    padding: EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
                   ),
-                  child: Text('Clear Cart'),
+                  child: Text(
+                    'Clear Cart',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                      fontSize: 16,
+                    ),
+                  ),
                 ),
-                SizedBox(height: 10),
                 ElevatedButton(
                   onPressed: () {
                     _showProductTypeDialog(context);
                   },
                   style: ElevatedButton.styleFrom(
                     backgroundColor: Colors.green,
+                    padding: EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
                   ),
-                  child: Text('Confirm'),
+                  child: Text(
+                    'Confirm',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                      fontSize: 16,
+                    ),
+                  ),
                 ),
               ],
             ),
